122104fc0cc9e115e5056b09b184471569a4adeb
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2022-06-25 11:18:55 -0300

    Melhoria na responsividade

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // Referencias
-let lista = document.querySelector('.container ol');
-let inputElementoLista = document.querySelector('main input');
-let botaoAdicionarTarefa = document.querySelector('main button');
-let botaoDeletarTodasTarefas = document.querySelector('.botao-deletar');
-let mostrarLista = document.querySelector('#view');
-let boxInput = document.querySelector('.box');
-
-// Funções
+const lista = document.querySelector('.container ol');
+const inputElementoLista = document.querySelector('main input');
+const botaoAdicionarTarefa = document.querySelector('main button');
+const botaoDeletarTodasTarefas = document.querySelector('.botao-deletar');
+const mostrarLista = document.querySelector('#view');
+const boxInput = document.querySelector('.box');
 var tarefas = JSON.parse(localStorage.getItem('lista_tarefas')) || [];
 
+// Funções
 const atualizarTarefas = () => {
     lista.innerHTML = '';
 
@@ -18,26 +17,25 @@ const atualizarTarefas = () => {
         var botaoDeletar = document.createElement('i');
         var indiceTarefa = tarefas.indexOf(tarefa);
 
-        botaoDeletar.setAttribute('onclick', 'deletarTarefa(' + indiceTarefa + ')');
-        botaoDeletar.classList.add('fa', 'fa-trash-o');
-
-        elementoTarefa.appendChild(textoTarefa);
-        lista.appendChild(elementoTarefa);
-        elementoTarefa.appendChild(botaoDeletar);
+        botaoDeletar.setAttribute('onclick', 'deletarTarefa(' + indiceTarefa + ')'); botaoDeletar.classList.add('fa', 'fa-trash-o');
+        elementoTarefa.appendChild(textoTarefa); lista.appendChild(elementoTarefa); elementoTarefa.appendChild(botaoDeletar);
     };
 };
+
+// Função auxiliar
+const focoInput = () => { if (window.innerWidth >= 992) { inputElementoLista.focus(); } else { }; };
 
 const adicionarTarefa = () => {
     var textoTarefa = inputElementoLista.value;
 
     tarefas.push(textoTarefa);
     inputElementoLista.value = '';
-    atualizarTarefas(); salvarNoStorage(); fimDaPagina(); inputElementoLista.focus();
+    atualizarTarefas(); salvarNoStorage(); fimDaPagina(); inputElementoLista.focus(); setTimeout(() => botaoAdicionarTarefa.disabled = true, 100);
 };
 
 const deletarTarefa = (indiceTarefa) => {
     tarefas.splice(indiceTarefa, 1);
-    atualizarTarefas(); salvarNoStorage(); inputElementoLista.focus()
+    atualizarTarefas(); salvarNoStorage(); focoInput();
 };
 
 const salvarNoStorage = () => {
@@ -48,48 +46,31 @@ const deletarTodasTarefas = () => {
     tarefas.splice(0, tarefas.length);
     atualizarTarefas();
     salvarNoStorage();
-    inputElementoLista.focus()
+    focoInput();
 };
 
-const ativarBotao = () => {
-    if (inputElementoLista.value.length <= 0) {
-        botaoAdicionarTarefa.disabled = true
-    } else { botaoAdicionarTarefa.disabled = false }
-};
+const ativarBotao = () => { botaoAdicionarTarefa.disabled = (inputElementoLista.value.length <= 0) ? true : false; };
 
-const fimDaPagina = () => {
-    window.scrollTo(0, document.body.scrollHeight);
-}
+const fimDaPagina = () => { window.scrollTo(0, document.body.scrollHeight); }
 
 // Eventos
-window.addEventListener('load', () => { atualizarTarefas(); inputElementoLista.focus(); });
-
+window.addEventListener('load', () => { atualizarTarefas(); focoInput(); });
+inputElementoLista.addEventListener('input', ativarBotao);
 botaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
 botaoDeletarTodasTarefas.addEventListener('click', deletarTodasTarefas);
-
 document.addEventListener('keydown', (event) => {
-    if (event.which == 13 && inputElementoLista.value.length > 0) {
-        adicionarTarefa();
-        setTimeout(() => botaoAdicionarTarefa.disabled = true, 100)
-    };
+    if (event.which == 13 && inputElementoLista.value.length > 0) { adicionarTarefa(); };
 });
-
-inputElementoLista.addEventListener('input', ativarBotao);
-botaoAdicionarTarefa.addEventListener('click', () => {
-    botaoAdicionarTarefa.disabled = true;
-});
-
 mostrarLista.addEventListener('click', () => {
     boxInput.classList.toggle('remove');
     if (mostrarLista.classList.contains("fa-eye")) {
         mostrarLista.classList.replace("fa-eye", "fa-eye-slash");
     } else {
         mostrarLista.classList.replace("fa-eye-slash", "fa-eye");
-    }
-})
+    };
+});
 
 setInterval(() => {
-    // Operadores ternários
     botaoDeletarTodasTarefas.style.display = (tarefas.length > 0) ? 'flex' : 'none';
     mostrarLista.style.display = (tarefas.length > 0) ? 'flex' : 'none';
-}, 10);
+}, 100);
